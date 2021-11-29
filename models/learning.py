4fc3797b0fa532fcbdd8dcf0e2183222a77542f6
Ammar Mian <ammar.mian@univ-savoie.fr>
2021-11-29 16:39:29 +0100

    Added structure

---
+++ models/learning.py
@@ -0,0 +1,11 @@
+'''
+File: learning.py
+Created Date: Monday November 29th 2021 - 12:07pm
+Author: Ammar Mian
+Contact: ammar.mian@univ-smb.fr
+-----
+Last Modified: Mon Nov 29 2021
+Modified By: Ammar Mian
+-----
+Copyright (c) 2021 Université Savoie Mont-Blanc
+'''
